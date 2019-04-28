1b23b5f0de6fdcb10cf25de2a658c1db8107406e
Daniel R. Pearson <faballion@gmail.com>
2019-04-29 07:09:46 +0900

    Make footer stick to the bottom of page

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -5,7 +5,24 @@ import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'jhi-main',
-    templateUrl: './main.component.html'
+    templateUrl: './main.component.html',
+    styles: [
+        `
+            #page-container {
+                position: relative;
+                min-height: 100vh;
+            }
+            #content-wrap {
+                padding-bottom: 4.5rem;
+            }
+            #footer {
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                height: 4.5rem;
+            }
+        `
+    ]
 })
 export class JhiMainComponent implements OnInit {
     constructor(private titleService: Title, private router: Router) {}
